844c15feb08a74819d6ef41cb0f319fab7c68d57
nonamejeff <nnjeff.prod@gmail.com>
2025-09-18 14:27:50 -0400

    added juce

---
+++ juce_port/build/CMakeFiles/SanctSoundJuceApp.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_graphics/juce_graphics_Sheenbidi.c" "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/build/CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_graphics/juce_graphics_Sheenbidi.c.o"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED=1"
+  "JUCE_MODAL_LOOPS_PERMITTED=1"
+  "JUCE_MODULE_AVAILABLE_juce_audio_basics=1"
+  "JUCE_MODULE_AVAILABLE_juce_audio_devices=1"
+  "JUCE_MODULE_AVAILABLE_juce_audio_formats=1"
+  "JUCE_MODULE_AVAILABLE_juce_audio_processors=1"
+  "JUCE_MODULE_AVAILABLE_juce_audio_utils=1"
+  "JUCE_MODULE_AVAILABLE_juce_core=1"
+  "JUCE_MODULE_AVAILABLE_juce_data_structures=1"
+  "JUCE_MODULE_AVAILABLE_juce_dsp=1"
+  "JUCE_MODULE_AVAILABLE_juce_events=1"
+  "JUCE_MODULE_AVAILABLE_juce_graphics=1"
+  "JUCE_MODULE_AVAILABLE_juce_gui_basics=1"
+  "JUCE_MODULE_AVAILABLE_juce_gui_extra=1"
+  "JUCE_STANDALONE_APPLICATION=1"
+  "JUCE_USE_CURL=0"
+  "JUCE_WEB_BROWSER=0"
+  "NDEBUG=1"
+  "_NDEBUG=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "SanctSoundJuceApp_artefacts/JuceLibraryCode"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/Source"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/VST3_SDK"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK/lv2"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK/serd"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK/sord"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK/sord/src"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK/sratom"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK/lilv"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/format_types/LV2_SDK/lilv/src"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_basics/juce_audio_basics.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_basics/juce_audio_basics.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_basics/juce_audio_basics.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_devices/juce_audio_devices.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_devices/juce_audio_devices.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_devices/juce_audio_devices.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_formats/juce_audio_formats.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_formats/juce_audio_formats.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_formats/juce_audio_formats.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/juce_audio_processors.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_processors/juce_audio_processors.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_processors/juce_audio_processors.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/juce_audio_processors_ara.cpp" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_processors/juce_audio_processors_ara.cpp.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_processors/juce_audio_processors_ara.cpp.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_audio_utils/juce_audio_utils.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_utils/juce_audio_utils.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_audio_utils/juce_audio_utils.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_core/juce_core.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_core/juce_core.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_core/juce_core.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_core/juce_core_CompilationTime.cpp" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_core/juce_core_CompilationTime.cpp.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_core/juce_core_CompilationTime.cpp.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_data_structures/juce_data_structures.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_data_structures/juce_data_structures.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_data_structures/juce_data_structures.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_dsp/juce_dsp.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_dsp/juce_dsp.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_dsp/juce_dsp.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_events/juce_events.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_events/juce_events.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_events/juce_events.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_graphics/juce_graphics.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_graphics/juce_graphics.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_graphics/juce_graphics.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_graphics/juce_graphics_Harfbuzz.cpp" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_graphics/juce_graphics_Harfbuzz.cpp.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_graphics/juce_graphics_Harfbuzz.cpp.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_gui_basics/juce_gui_basics.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_gui_basics/juce_gui_basics.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_gui_basics/juce_gui_basics.mm.o.d"
+  "/Users/kailashpermaul/Documents/GitHub/semi/juce_port/external/JUCE/modules/juce_gui_extra/juce_gui_extra.mm" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_gui_extra/juce_gui_extra.mm.o" "gcc" "CMakeFiles/SanctSoundJuceApp.dir/external/JUCE/modules/juce_gui_extra/juce_gui_extra.mm.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ juce_port/build/CMakeFiles/SanctSoundJuceApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SanctSoundJuceApp.
+# This may be replaced when dependencies are built.
